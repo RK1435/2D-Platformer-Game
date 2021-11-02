759275faa61c617314e40de641ba9bd9f0411203
RK1435 <46847867+RK1435@users.noreply.github.com>
2021-11-02 23:31:31 +0530

    Physics and Script
    Physics for the Player and platform has updated, Script for Left and Right button animations has updated.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,12 +4,31 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public Animator animator;
     private void Awake()
     {
         Debug.Log("Player Controller Awake!!!");
     }
-    private void OnCollisionEnter2D(Collision2D collision)
+    /*private void OnCollisionEnter2D(Collision2D collision)
     {
         Debug.Log("Collision:" + collision.gameObject.name);
+    }*/
+
+    private void Update()
+    {
+        float speed = Input.GetAxisRaw("Horizontal");
+        animator.SetFloat("Speed",  Mathf.Abs(speed));
+
+        Vector3 scale = transform.localScale;
+        if (speed < 0)
+        {
+           scale.x = -1f * Mathf.Abs(scale.x);
+        }
+        else if(speed > 0)
+        {
+            scale.x = Mathf.Abs(scale.x);
+        }
+        transform.localScale = scale;
     }
+
 }
